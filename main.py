0359da4cf3c86bf92b02af1d582d36da25ffd6df
EgorNifakin <enifakin@list.ru>
2023-02-08 21:38:08 +0300

    Rewriting on PyQt5

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-import cv2
-from kivy.app import App
-from mainWindow import MainWindow
+# import cv2
+# from kivy.app import App
+# from mainWindow import MainWindow
+# from kivy.core.window import Window
+import sys
+from PyQt5 import QtWidgets, uic
+
+qtcreator_file  = "frame1.ui" # Enter file here.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
 
-def show_video():
-    video = cv2.VideoCapture(source='Sources/forest1080.mp4')
-    while video.isOpened():
-        ret, frame = video.read()
-        cv2.imshow('test', frame)
-        print(video.get(5))
-        if cv2.waitKey(1) == ord('q'):
-            break
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
 
+        self.choise_video_btn.clicked.connect(self.printing)
 
-class App(App):
-    def build(self):
-        return MainWindow()
-        # folder = filedialog.askopenfilename()
-        # if folder != "":
-        #     print(folder)
-        #     video = Video(source = 'Sources/forest1080.mp4')
-        #     video.state = "play"
-        #     video.options = {'eos':'loop'}
-        #     video.allow_stretch = True
-        #     return video
-        # else:
-        #     print("ERR")
+    
+    def printing(self):
+        print("asd")
+
 
 
 if __name__ == "__main__":
-    App().run()
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
+
+# # def show_video():
+# #     video = cv2.VideoCapture(source='Sources/forest1080.mp4')
+# #     while video.isOpened():
+# #         ret, frame = video.read()
+# #         cv2.imshow('test', frame)
+# #         print(video.get(5))
+# #         if cv2.waitKey(1) == ord('q'):
+# #             break
+
+
+# class App(App):
+#     def build(self):
+#         Window.size = (800, 700)
+#         return MainWindow()
+
+
+
+# if __name__ == "__main__":
+#     App().run()
--- mainWindow.py
+++ mainWindow.py
@@ -2,18 +2,27 @@ from kivy.uix.boxlayout import BoxLayout
 from tkinter import filedialog
 
 
+def choose_file() -> str:
+    """Open file dialog and choose file"""
+    file_source = filedialog.askopenfilename()
+    if file_source != "":
+        return file_source
+
+
 class MainWindow(BoxLayout):
     def __init__(self, **kwargs) -> None:
         """Constructor"""
         super().__init__(**kwargs)
+        self._video_source = None
+        self._photo_source = None
 
     def choose_video_click(self) -> None:
-        """Comment"""
-        folder = filedialog.askopenfilename()
-        if folder != "":
-            print(folder)
-            # self.ids.searching_image.source = folder
-            self.ids.main_video.source = folder
+        """fill video source"""
+        self.ids.main_video.source = choose_file()
+
+    def choose_image_click(self) -> None:
+        """fill image source"""
+        self.ids.searching_image.source = choose_file()
 
 #
 # def test():
