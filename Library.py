f2c238a984f1612a2ff38574a6a9bd7a62fec360
EgorNifakin <enifakin@list.ru>
2023-02-13 14:11:54 +0300

    Edit alg and design

--- Library.py
+++ Library.py
@@ -1,39 +1,5 @@
 import cv2
 import numpy as np
-def sift_detector(new_image, image_template):
-    # Function that compares input image to template
-    # It then returns the number of SIFT matches between them
-    image1 = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
-    image2 = image_template
-
-    # Create SIFT detector object
-    #sift = cv2.SIFT()
-    sift = cv2.xfeatures2d.SIFT_create()
-    # Obtain the keypoints and descriptors using SIFT
-    keypoints_1, descriptors_1 = sift.detectAndCompute(image1, None)
-    keypoints_2, descriptors_2 = sift.detectAndCompute(image2, None)
-
-    # Define parameters for our Flann Matcher
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 3)
-    search_params = dict(checks = 100)
-
-    # Create the Flann Matcher object
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-    # Obtain matches using K-Nearest Neighbor Method
-    # the result 'matchs' is the number of similar matches found in both images
-    matches = flann.knnMatch(descriptors_1, descriptors_2, k=2)
-
-    # Store good matches using Lowe's ratio test
-    good_matches = []
-    for m,n in matches:
-        if m.distance < 0.7 * n.distance:
-            good_matches.append(m) 
-    return len(good_matches)
-    
-
-
 
 class Library:
     def __init__(self):
@@ -57,52 +23,23 @@ class Library:
         self._image_source = value
 
     def show_video(self):
-      
-        # img_rgb = cv2.imread('Sources/Screenshot_1.png')
-        # img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-        # template = cv2.imread('Sources/Screenshot_3.png',0)
-        # w, h = template.shape[::-1]
-
-        # res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-        # threshold = 0.8
-        # loc = np.where( res >= threshold)
-        # for pt in zip(*loc[::-1]):
-        #     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-        # cv2.imwrite('res.png',img_rgb)
-
-        # img_rgb = cv2.imread('Sources/Screenshot_1.png')
-        # img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
         video = cv2.VideoCapture(self._video_source)
-        template = cv2.imread(self._image_source,0)
-        w, h = template.shape[::-1]
+        template= cv2.imread(self._image_source,0)
         while video.isOpened():
             ret, frame = video.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
-            threshold = 0.63
-            loc = np.where( res >= threshold)
+            gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            result= cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+            loc = np.where(result >= 0.8)
+            height, width= template.shape[:2]
             for pt in zip(*loc[::-1]):
-                cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+                cv2.rectangle(frame, pt, (pt[0] + width, pt[1] + height), (0, 0, 255), 1)
+            # min_val, max_val, min_loc, max_loc= cv2.minMaxLoc(result)
+            # height, width= template.shape[:2]
+            # top_left= max_loc
+            # bottom_right= (top_left[0] + width, top_left[1] + height)
+            # cv2.rectangle(frame, top_left, bottom_right, (0,0,255),1)
 
-            cv2.imshow('res.png',frame)
+            cv2.imshow('showing', frame)
             if cv2.waitKey(1) == ord('q'):
                 cv2.destroyAllWindows()
                 break
-
-
-
-
-
-
-
-
-# # def show_video():
-# #     video = cv2.VideoCapture(source='Sources/forest1080.mp4')
-# #     while video.isOpened():
-# #         ret, frame = video.read()
-# #         cv2.imshow('test', frame)
-# #         print(video.get(5))
-# #         if cv2.waitKey(1) == ord('q'):
-# #             break
--- main.py
+++ main.py
@@ -24,6 +24,14 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.mediaPlayer = self.makeMediaPlayer()
         self.mediaPlayer.setVolume(0)
 
+        self.play_btn.setVisible(False)
+        self.pause_btn.setVisible(False)
+        self.restart_btn.setVisible(False)
+
+        self.play_btn.clicked.connect(self.play_btn_click)
+        self.pause_btn.clicked.connect(self.pause_btn_click)
+        self.restart_btn.clicked.connect(self.replay_btn_click)
+
         self.video_path.textChanged[str].connect(self.on_text_changed)
         self.image_path.textChanged[str].connect(self.on_text_changed)
 
@@ -48,7 +56,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         file_path, _ = QFileDialog.getOpenFileName(self, 'Open file', None, "Image (*.png *.jpg *jpeg)")
         if file_path != "":
             self._Library.image_source = file_path
-
             self.image_path.setText(file_path)
             self.show_image.setPixmap(QPixmap(file_path))
 
@@ -57,24 +64,47 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         if file_path != "":
             self._Library.video_source = file_path
 
-            print(self._Library.video_source)
             self.video_path.setText(file_path)
 
             self.mediaPlayer.setMedia(QMediaContent(QUrl(file_path)))
             self.mediaPlayer.play()
+            self.mediaPlayer.pause()
+            self.play_btn.setVisible(True)
+            self.pause_btn.setVisible(True)
+            self.restart_btn.setVisible(True)
+
+    
+    def play_btn_click(self):
+        self.mediaPlayer.play()
+    def pause_btn_click(self):
+        self.mediaPlayer.pause()
+    
+    def replay_btn_click(self):
+        self.mediaPlayer.stop()
+        self.mediaPlayer.play()
+
+            
 
     def on_text_changed(self, text):
+        if self.video_path.text() == "":
+            self.play_btn.setVisible(False)
+            self.pause_btn.setVisible(False)
+            self.restart_btn.setVisible(False)
+        else:
+            self.play_btn.setVisible(True)
+            self.pause_btn.setVisible(True)
+            self.restart_btn.setVisible(True)
+
+
         if (self.video_path.text() != "") and (self.image_path.text() != ""):
             self.but_execute.setEnabled(True)
         else:
             self.but_execute.setEnabled(False)
 
+
+
     def but_execute_click(self):
         self._Library.show_video()
-
-
-# create second fuct for textChandeg
-# insert in 2 func try exepct 
 
 
 if __name__ == "__main__":
@@ -82,12 +112,3 @@ if __name__ == "__main__":
     window = MyApp()
     window.show()
     sys.exit(app.exec_())
-
-# # def show_video():
-# #     video = cv2.VideoCapture(source='Sources/forest1080.mp4')
-# #     while video.isOpened():
-# #         ret, frame = video.read()
-# #         cv2.imshow('test', frame)
-# #         print(video.get(5))
-# #         if cv2.waitKey(1) == ord('q'):
-# #             break
